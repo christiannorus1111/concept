39e7a4ab139848c9f1c56ae8d7fec396197a05da
Your Name <you@example.com>
2018-01-29 15:31:51 +0100

    sub

--- src/GameTest.java
+++ src/GameTest.java
@@ -1,15 +1,15 @@
 import java.time.LocalDateTime;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
-import java.util.Random;
 
 public class GameTest {
 
     public static LocalDateTime oldDate = LocalDateTime.now();
+    private Field field;
 
     public void play(int width, int height, int numberOfPlayer, int privateZone, long moveInterval, long timout) {
-        Field field = new Field(width, height, numberOfPlayer, privateZone, moveInterval, timout);
+        field = new Field(width, height, numberOfPlayer, privateZone, moveInterval, timout);
         Thread thread = new Thread(field, "field");
         thread.start();
 }
@@ -17,25 +17,22 @@ public class GameTest {
     public void testGame() {
         /* The rules specify a 100 x 100 meter field and move interval 1000 ms and timeout 10000 ms bur for
         faster regression test this is reduced */
-        GameTest game = new GameTest();
-        try {
-            game.play(30, 30, 10, 2, 100, 1000);
-        } catch (Exception ex) {
-            System.out.println("Error in game" + ex.getMessage());
+
+        for(int i = 0; i < 100; i++) {
+            GameTest game = new GameTest();
+            try {
+                game.play(50, 50, 10, 2, 100, 1000);
+                // TODO check old games and compare to make sure player objects are not identical to make sure game is random
+            } catch (Exception ex) {
+                System.out.println("Error in game" + ex.getMessage());
+            }
         }
     }
 
     public static void main(String[] args) {
         GameTest game = new GameTest();
         try {
-            //game.play(100, 100, 10, 2, 1000, 10000);
-            Random rn = new Random();
-
-            for(int i =0; i < 100; i++)
-            {
-                int answer = rn.nextInt(10) + 1;
-                System.out.println(answer);
-            }
+            game.play(100, 100, 10, 2, 1000, 10000);
         } catch (Exception ex) {
             System.out.println("Error in game" + ex.getMessage());
         }
